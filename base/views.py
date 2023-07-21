2da2f8a2f8b14f698d7e62b2909d86c558a20e13
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-21 05:43:49 +0100

    commit

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LoginView
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
+    fields= '__all__'
 
 def home(request):
     return HttpResponse('To do list')
