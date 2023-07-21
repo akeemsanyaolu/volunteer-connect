debfb4e81c39d3ebd53e319774ec62962ffdd821
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-21 05:41:36 +0100

    commit

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+
 def home(request):
     return HttpResponse('To do list')
