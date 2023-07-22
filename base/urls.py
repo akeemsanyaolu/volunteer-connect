1eb0164fadb3e04a4689990e60009972f100d122
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-22 09:55:37 +0100

    commit

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import CustomLoginView
 
 urlpatterns = [
-    path('', views.home, name='home')
+    #path('', views.home, name='home')
+    path('login/', CustomLoginView, name='login')
 ]
