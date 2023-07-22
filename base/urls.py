430665ea9cafefac7b4ba495885c7befbd23bff7
Akeem Sanyaolu <akeemsanyaolu2@gmail.com>
2023-07-22 10:00:03 +0100

    commit

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import CustomLoginView
+from .views import CustomLoginView, home
 
 urlpatterns = [
-    #path('', views.home, name='home')
-    path('login/', CustomLoginView, name='login')
+    path('', home, name='home'),
+    path('login/', CustomLoginView.as_view(), name='login')
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
+from django.views.generic.list import ListView
 
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields= '__all__'
+    
 
 def home(request):
     return HttpResponse('To do list')
